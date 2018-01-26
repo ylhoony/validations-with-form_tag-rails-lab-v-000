a235b07ece19493331eef2951cecf2a60d7bd278
Hoon Lee <yl.hoony@gmail.com>
2018-01-26 22:09:26 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,5 @@
 class AuthorsController < ApplicationController
-  def show
-    @author = Author.find(params[:id])
-  end
-
+  
   def new
     @author = Author.new
   end
@@ -10,7 +7,16 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create(author_params)
 
-    redirect_to author_path(@author)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @author = Author.find(params[:id])
   end
 
   private
